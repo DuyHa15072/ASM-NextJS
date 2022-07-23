b1be26458ef619dad633f33fb5e43198ad25682b
daibede <xuandai300092@gmail.com>
2022-07-23 10:43:30 +0700

    update

--- my-app/pages/index.tsx
+++ my-app/pages/index.tsx
@@ -64,7 +64,9 @@ const Home: NextPage = () => {
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
+        <h1>a</h1>
       </footer>
+    
     </div>
   )
 }
