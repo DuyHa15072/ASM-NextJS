78de2f8783dc4b23c04c718ab9117743084e62de
Duy Ha <havanduy@gmail.com>
2022-07-26 00:11:44 +0700

    interface signin, signup

---
+++ my-app/components/layout/login.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { LayoutProps } from '../../model/layout'
+
+type Props = {}
+
+const Login = ({children}: LayoutProps) => {
+  return (
+    <div>
+        {children}
+    </div>
+  )
+}
+
+export default Login
