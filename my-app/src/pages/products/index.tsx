4679983b9ed9fcba5adde7b53efe7c59070342e0
daibede <xuandai300092@gmail.com>
2022-07-30 22:40:44 +0700

    update detail

--- my-app/src/pages/products/index.tsx
+++ my-app/src/pages/products/index.tsx
@@ -78,9 +78,9 @@ const Products = () => {
                     <div className="bg-white">
                         <div className="max-w-2xl mx-auto px-4 sm:px-6 lg:max-w-7xl lg:px-8">
                             <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-2 xl:gap-x-8">
-                                {data?.map((item, index) => {
+                                {data?.map((item : any, index : any) => {
                                     return (
-                                        <a href={`/product/${item._id}`} key={index} className="group">
+                                        <a href={`/products/${item._id}`} key={index} className="group">
                                             <div className="w-full md:h-[300px] aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
                                                 <img src={item.img} alt="Tall slender porcelain bottle with natural clay textured body and cork stopper." className="w-full h-full object-center object-cover group-hover:opacity-75" />
                                             </div>
