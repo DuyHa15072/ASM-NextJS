ef6b392945986c671ee248b7b40aece82cf456bf
daibede <xuandai300092@gmail.com>
2022-08-04 23:01:01 +0700

    fix bug add products

--- my-app/server/src/models/products.js
+++ my-app/server/src/models/products.js
@@ -2,31 +2,31 @@
 import categoryProducts from './categoryProducts';
 import mongoose, { ObjectId } from 'mongoose';
 
-const productSchema = new mongoose.Schema(
-  {
-    id: {
-      type: String,
-    },
-    name: {
-      type: String,
-      required: true,
-      minLength: 5,
-      maxLength: 32,
-    },
-    img: {
-      type: String,
-      required: true,
-    },
-    price: {
-      type: String,
-      required: true,
-    },
-    description: {
-      type: String,
-      required: true,
-    },
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  } ,
+  img: {
+    type: String,
+    required: true,
+  } ,
+  price: {
+    type: Number,
+    required: true
+  } ,
+  quantity: {
+    type: Number,
+    required: true
+  } ,
+  desc: {
+    type: String,
+    required: true,
   },
-  { timestamps: true }
-);
+  category: {
+      type: ObjectId,
+      ref: categoryProducts
+  }
+}, { timestamps: true })
 
 export default mongoose.model('Product', productSchema);
