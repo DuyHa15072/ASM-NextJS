e95ff35554d111823188fa1b3d2b2714a1144cb6
Duy Ha <havanduy@gmail.com>
2022-08-05 14:04:39 +0700

    N1-49 api user list, edit ,delete

--- my-app/server/src/routes/auth.js
+++ my-app/server/src/routes/auth.js
@@ -2,10 +2,14 @@
 
 import express from 'express';
 import { signin, signup } from '../controllers/auth';
+import { deleteUser, listUser, updateUser } from '../controllers/user';
 
 const router = express.Router();
 
 router.post('/signup', signup);
 router.post('/signin', signin);
+router.get('/users', listUser);
+router.delete('/users/:id', deleteUser);
+router.put('/users/:id', updateUser);
 
 export default router;
